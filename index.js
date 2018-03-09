1c3dfd31d33c53128dcf7e7dd7a93e31e4f0d6c5
abra2abra <abra2abra@mailinator.com>
2018-03-09 20:36:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,11 +11,10 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
 
 function deleteFromObjectByKey(object, key){
   var jj = Object.assign({}, object);
- delete jj.key;
+ delete jj[key];
   return jj;
 }
-var playground = {ha: 'one', haha: 'two'};
-console.log(deleteFromObjectByKey(playground, playground.ha));
+
 
 function destructivelyDeleteFromObjectByKey(obj, key){
   delete obj[key];
